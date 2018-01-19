b7cffb117d4520fa10153fa0c39dfbe0cbd6415e
Juan Treminio <jtreminio@gmail.com>
2018-01-18 22:57:25 -0600

    concat::fragment no longer has ensure parameter

--- manifests/resource/mailhost.pp
+++ manifests/resource/mailhost.pp
@@ -128,7 +128,6 @@ define nginx::resource::mailhost (
 
   if ($listen_port != $ssl_port) {
     concat::fragment { "${name}-header":
-      ensure  => present,
       target  => $config_file,
       content => template('nginx/mailhost/mailhost.erb'),
       order   => '001',
@@ -138,7 +137,6 @@ define nginx::resource::mailhost (
   # Create SSL File Stubs if SSL is enabled
   if ($ssl) {
     concat::fragment { "${name}-ssl":
-      ensure  => present,
       target  => $config_file,
       content => template('nginx/mailhost/mailhost_ssl.erb'),
       order   => '700',
